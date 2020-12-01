bd6ab5f7118363beab18d0d00db71da05a387257
Pierre Henry <pierre.henry21@outlook.fr>
2020-12-01 15:09:50 +0100

    Creating all static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, path: '/profile'
   root 'static_pages#index'
   resource :users, path: '/profile'
+  get '/contact', to: 'static_pages#contact'
+  get '/histoire', to: 'static_pages#about'
+  get '/informations', to: 'static_pages#info'
+  get '/galerie', to: 'static_pages#galerie'
 end
