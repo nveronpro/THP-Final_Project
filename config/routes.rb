96d869f0de072d0e4e71128f07b277afcf2cf9d0
Nico <arrvac@gmail.com>
2020-12-02 16:10:56 +0100

    Front and back work on the item show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '/profile'
+  resource :users, path: '/profile'
+
   resources :orders
   resources :items
-  devise_for :users, path: '/profile'
-  root 'static_pages#index'
-  resource :users, path: '/profile'
+
   get '/contact', to: 'static_pages#contact'
   get '/histoire', to: 'static_pages#about'
   get '/informations', to: 'static_pages#info'
   get '/galerie', to: 'static_pages#galerie'
+  get '/acceuil', to: 'static_pages#index'
+  get '/abonemments', to: 'items#index'
+
+  root 'static_pages#index'
 end
