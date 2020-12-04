985499c47407ff4cf5e36411cec939d139de3edc
Pierre Henry <pierre.henry21@outlook.fr>
2020-12-04 11:48:44 +0100

    Add action mailer and welcome mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.mailer[:sendgrid_login],
+  :password => Rails.application.credentials.mailer[:sendgrid_pwd],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
