783aef7635673eba161024eb96cc158b56a0cf6f
Pierre Henry <pierre.henry21@outlook.fr>
2020-12-04 14:58:40 +0100

    Add action mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :cart_create
-  after_create :welcome_send
+  after_create :cart_create, :welcome_send
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   
 end
