0987de44c83cd172a697e9c23afaec679e05a670
Pierre Henry <pierre.henry21@outlook.fr>
2020-12-04 15:34:09 +0100

    Working mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@yopmail.com'
+  default from: 'pierrehenr@hotmail.fr'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
