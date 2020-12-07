7e34c88c6da857db388eef4b3fe3551d3db5414a
Pierre Henry <pierre.henry21@outlook.fr>
2020-12-07 10:03:10 +0100

    Galery page + JS with asset pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -56,17 +56,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_140515) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "item_carts", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
